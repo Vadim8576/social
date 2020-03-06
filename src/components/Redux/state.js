951516e359c9fc42efea817bb6b6af279167ac45
vadim8576 <vr-project@yandex.ru>
2020-03-06 19:42:49 +0300

    Lesson 37

--- src/components/Redux/state.js
+++ src/components/Redux/state.js
@@ -1,58 +1,61 @@
-let rerenderEntireTree = () => {
-    console.log('state change');
-}
-
-
-const state = {
-    dialogsPage: {
-        users: [
-            {id: 1, name: 'Андрей'},
-            {id: 2, name: 'Валера'},
-            {id: 3, name: 'Анна'},
-            {id: 4, name: 'Сергей'}],
-        messages: [
-            {id: 1, messages: 'Привет!'},
-            {id: 2, messages: 'Как дела?'},
-            {id: 3, messages: 'Yo!'},
-            {id: 4, messages: 'Пока!'}],
+let store = {
+    _state: {
+        dialogsPage: {
+            users: [
+                {id: 1, name: 'Андрей'},
+                {id: 2, name: 'Валера'},
+                {id: 3, name: 'Анна'},
+                {id: 4, name: 'Сергей'}],
+            messages: [
+                {id: 1, messages: 'Привет!'},
+                {id: 2, messages: 'Как дела?'},
+                {id: 3, messages: 'Yo!'},
+                {id: 4, messages: 'Пока!'}],
+        },
+        postsPage: {
+            posts: [
+            {id: 1, message: 'Hello!', likesCount: 10},
+            {id: 2, message: 'Это переданный...', likesCount: 0},
+            {id: 3, message: '...параметр Props,', likesCount: 5},
+            {id: 4, message: 'а это значение переменной...', likesCount: 24}],
+            newPostText: ''
+        }
     },
-    postsPage: {
-        posts: [
-        {id: 1, message: 'Hello!', likesCount: 10},
-        {id: 2, message: 'Это переданный...', likesCount: 0},
-        {id: 3, message: '...параметр Props,', likesCount: 5},
-        {id: 4, message: 'а это значение переменной...', likesCount: 24}],
-        newPostText: ''
+
+    getState() {
+        return this._state;
+    },
+
+    _callSubscriber() {
+        console.log('state change');
+    },
+    
+    addPost() {
+ 
+        if(!this._state.postsPage.newPostText) return;
+        
+        
+        let newPost = {
+            id: 5,
+            message: this._state.postsPage.newPostText,
+            likesCount: 0
+        };
+    
+        this._state.postsPage.posts.push(newPost);
+        this._state.postsPage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+
+    updateNewPostText(newText) {  
+        this._state.postsPage.newPostText = newText;
+        this._callSubscriber(this._state);
+        
+    },
+
+    subscribe(observer) {
+        this._callSubscriber = observer; // observer (наблюдатель) - это pattern
     }
-};
-
-
-export const addPost = () => {
- 
-    if(!state.postsPage.newPostText) return;
-    
-    
-    let newPost = {
-        id: 5,
-        message: state.postsPage.newPostText,
-        likesCount: 0
-    };
-
-    state.postsPage.posts.push(newPost);
-    state.postsPage.newPostText = '';
-    rerenderEntireTree(state);
 }
 
-export const updateNewPostText = (newText) => {
- 
-    
-    state.postsPage.newPostText = newText;
-    rerenderEntireTree(state);
-    
-}
 
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer; // observer (наблюдатель) - это pattern
-}
-
-export default state;
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import './index.css';
 
 import * as serviceWorker from './serviceWorker';
-import state, { subscribe } from './components/Redux/state'
+import store from './components/Redux/state'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { addPost } from './components/Redux/state';
-import { updateNewPostText } from './components/Redux/state';
 import { BrowserRouter } from 'react-router-dom';
 
 
@@ -17,15 +15,15 @@ export let rerenderEntireTree = (state) => {
         <BrowserRouter>
             <App
                 state={state}
-                addPost={addPost}
-                updateNewPostText={updateNewPostText} />
+                addPost={store.addPost.bind(store)}
+                updateNewPostText={store.updateNewPostText.bind(store)} />
         </BrowserRouter>, document.getElementById('root'));
 }
 
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
