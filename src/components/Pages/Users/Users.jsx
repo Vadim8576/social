757e007b01d5f81c3814f00e077c62f98acb98b6
Vadim8576 <61579030+Vadim8576@users.noreply.github.com>
2020-03-12 20:53:00 +0300

    lesson 51

--- src/components/Pages/Users/Users.jsx
+++ src/components/Pages/Users/Users.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import css from './users.module.css';
+import * as axios from 'axios';
 
 let Users = (props) => {
 
+    console.log(props.users.length);
+
     if(props.users.length === 0) {
-        props.setUsers([
-            {id: 1, photoUrl: 'images/UserPhoto.jpg', followed: true, fullName: 'Vadim', status: 'I am a boss', location: {city: 'SPb', country: 'Russia'} },
-            {id: 2, photoUrl: 'images/UserPhoto.jpg', followed: false, fullName: 'Sergey', status: 'I am a driver', location: {city: 'Moscow', country: 'Russia'} },
-            {id: 3, photoUrl: 'images/UserPhoto.jpg', followed: false, fullName: 'Nikolas', status: 'Hello!', location: {city: 'LA', country: 'USA'} }
-        ])
-    };
+        axios
+            .get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                props.setUsers(response.data.items);      
+            })
+    }
+
+    // if(props.users.length === 0) {
+    //     props.setUsers([
+    //         {id: 1, photoUrl: 'images/UserPhoto.jpg', followed: true, fullName: 'Vadim', status: 'I am a boss', location: {city: 'SPb', country: 'Russia'} },
+    //         {id: 2, photoUrl: 'images/UserPhoto.jpg', followed: false, fullName: 'Sergey', status: 'I am a driver', location: {city: 'Moscow', country: 'Russia'} },
+    //         {id: 3, photoUrl: 'images/UserPhoto.jpg', followed: false, fullName: 'Nikolas', status: 'Hello!', location: {city: 'LA', country: 'USA'} }
+    //     ])
+    // };
 
     return <div>
         {
             props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
-                            <img src={u.photoUrl} className={css.avatar} />
+                            <img src={u.photos.small != null ? u.photos.small: 'images/UserPhoto.jpg'} className={css.avatar} />
                         </div>
                         <div>
                             {u.followed ?
@@ -26,12 +37,12 @@ let Users = (props) => {
                     </span>
                     <span>
                         <span>
-                            <div>{u.fullName}</div>
-                            <div>{u.status}</div>
+                            <div>{'u.fullName'}</div>
+                            <div>{'u.status'}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{'u.location.country'}</div>
+                            <div>{'u.location.city'}</div>
                         </span>
                     </span>
                 </div>)
