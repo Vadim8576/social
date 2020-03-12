23ac877b2247d8e3f27245f52f33fd85007ad930
Vadim8576 <61579030+Vadim8576@users.noreply.github.com>
2020-03-12 13:10:50 +0300

    lesson 48

--- src/components/Redux/postsReducer.js
+++ src/components/Redux/postsReducer.js
@@ -14,31 +14,23 @@ let initialState = {
 const postsReducer = (state = initialState, action) => {
     // console.log(state);
     switch(action.type) {
-        case ADD_POST: {
-            // if(!state.newPostText) return state;
-            let newPost = {
-                id: 5,
-                message: state.newPostText,
-                likesCount: 0
+        case ADD_POST:
+            return {
+                ...state,
+                posts: [
+                    ...state.posts,
+                    {id: 5, message: state.newPostText, likesCount: 0}
+                ],
+                newPostText: ''
             };
-            
-            
-            let stateCopy = {...state};
-            stateCopy.posts = [...state.posts];
-            stateCopy.posts.push(newPost);
-            stateCopy.newPostText = '';
-            return stateCopy;
-        }
-        case UPDATE_NEW_POST_TEXT: {
-            let stateCopy = {...state};
-            stateCopy.newPostText = action.newText;
-            return stateCopy; 
-        }
+        case UPDATE_NEW_POST_TEXT: 
+            return {
+                ...state,
+                newPostText: action.newText
+            };
         default:
         return state;
-    
     }
-
 }
 
 export const addPostActionCreator = () => ( {type: ADD_POST} );
