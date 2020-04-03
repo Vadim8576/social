22355ed1cbab224d2952cb196079537b7d2d1059
Vadim8576 <61579030+Vadim8576@users.noreply.github.com>
2020-04-03 19:06:18 +0300

    lesson 91

--- src/components/redux/reduxStore.js
+++ src/components/redux/reduxStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import profileReducer from "./profileReducer";
 import dialogsReducer from "./dialogsReducer";
 import usersReducer from "./usersReducer";
@@ -7,7 +7,7 @@ import thunkMiddleware from 'redux-thunk';
 import { reducer as formReducer } from 'redux-form';
 import appReducer from './appReducer';
 
-let redusers = combineReducers({
+let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
@@ -17,10 +17,21 @@ let redusers = combineReducers({
 });
 
 
-let store = createStore(redusers, applyMiddleware(thunkMiddleware));
+// Это настройка Redux DevTools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+  ));
+
+
+  
+// Использовать это создание store, если не юзать Redux DevTools
+// let store = createStore(redusers, applyMiddleware(thunkMiddleware));
+
+
 // Применяя параметр applyMiddleware,
 // мы говорим Store - прими промежуточные слои
 
-window.store = store;
+// window.store = store;
 
 export default store;
