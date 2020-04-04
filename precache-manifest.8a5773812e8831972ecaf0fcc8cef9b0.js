a84082aee52d61dc3e78ca9aab32d8f6242a529c
Vadim8576 <61579030+Vadim8576@users.noreply.github.com>
2020-04-04 19:13:52 +0300

    Updates

---
+++ precache-manifest.8a5773812e8831972ecaf0fcc8cef9b0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a386ef279375aa8f9d7b7b7a2554cb",
+    "url": "/social/index.html"
+  },
+  {
+    "revision": "b9c481ca9b9e62cdb499",
+    "url": "/social/static/css/4.4da9a799.chunk.css"
+  },
+  {
+    "revision": "3c606ab220f739f16a37",
+    "url": "/social/static/css/5.75433d19.chunk.css"
+  },
+  {
+    "revision": "730922c3596757c8edad",
+    "url": "/social/static/css/6.b994a4ab.chunk.css"
+  },
+  {
+    "revision": "0bfb7ee5708846dea5bd",
+    "url": "/social/static/css/7.33462230.chunk.css"
+  },
+  {
+    "revision": "071a20d22a9eeb0c12b5",
+    "url": "/social/static/css/main.2b5d2353.chunk.css"
+  },
+  {
+    "revision": "1b4e70783d2fd694fc02",
+    "url": "/social/static/js/0.dacfd89f.chunk.js"
+  },
+  {
+    "revision": "a50647b353fa5f7dbc91",
+    "url": "/social/static/js/3.558bd4ed.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/social/static/js/3.558bd4ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c481ca9b9e62cdb499",
+    "url": "/social/static/js/4.d6993b90.chunk.js"
+  },
+  {
+    "revision": "3c606ab220f739f16a37",
+    "url": "/social/static/js/5.eee15075.chunk.js"
+  },
+  {
+    "revision": "730922c3596757c8edad",
+    "url": "/social/static/js/6.2f70c0c8.chunk.js"
+  },
+  {
+    "revision": "0bfb7ee5708846dea5bd",
+    "url": "/social/static/js/7.f8fa2594.chunk.js"
+  },
+  {
+    "revision": "071a20d22a9eeb0c12b5",
+    "url": "/social/static/js/main.bdbeb250.chunk.js"
+  },
+  {
+    "revision": "8aa0adf282a2e9b52b55",
+    "url": "/social/static/js/runtime-main.b76c3a15.js"
+  },
+  {
+    "revision": "b5a04f51b66500e378070a3701f86506",
+    "url": "/social/static/media/UserPhoto.b5a04f51.jpg"
+  },
+  {
+    "revision": "d452daacda184e8b3463441c6943d9dd",
+    "url": "/social/static/media/loader.d452daac.gif"
+  }
+]);
